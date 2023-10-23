298d3562989865ebef9338269e1d712b476c4c53
Aziz Furkan Talşık <115635976+quanfr@users.noreply.github.com>
2023-10-23 23:10:51 +0300

    c# ilk calisma

---
+++ iki sayinin toplami/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("iki sayiyi toplayan program");
+Console.Write("1. sayiyi giriniz :");
+int sayi1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("2. sayiyi giriniz :");
+int sayi2 = Convert.ToInt16(Console.ReadLine());
+
+int toplam = sayi1 + sayi2;
+Console.Write("toplam = {0} + {1} = {2}", sayi1, sayi2, toplam);
+Console.ReadKey();
---
+++ not hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿{
+    double v, f, ort;
+    Console.WriteLine("Vize Giriniz ");
+
+    v = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Final Giriniz ");
+
+    f = Convert.ToDouble(Console.ReadLine());
+
+
+    ort = (v * 0.4) + (f * 0.6);
+    if (ort >= 70)
+    {
+        Console.WriteLine("Gecti");
+
+    }
+    else
+    {
+        Console.WriteLine("Kaldi");
+
+        Console.ReadKey();
+
+    }
+}
